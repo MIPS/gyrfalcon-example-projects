e1c0fcb4eb65af7a12c24d2a327ff411f5ccf362
Kevin Mills <kmills@mips.com>
2025-01-29 13:08:04 -0800

    check compiler found

--- config_rv64.mk
+++ config_rv64.mk
@@ -1,7 +1,13 @@
 RISCV_PATH=${HOME}/toolchain/riscv-elf/v2.05/bin
-CC = ${RISCV_PATH}/riscv64-mti-elf-gcc
+COMPILER = riscv64-mti-elf-gcc
 
-ifeq ($(RISCV_PATH),)
-# If RISCV_PATH is not defined, then it is assumed that the bin folder is on your path.
-CC = riscv64-mti-elf-gcc
+CC = ${RISCV_PATH}/$(COMPILER)
+
+ifeq (,$(wildcard $(CC)))
+# Explicit compiler not found.
+$(info Assuming compiler is on your path)
+CC = $(COMPILER)
 endif
+
+require:
+	@$(CC) --version || (echo ERROR: $(CC) not found, check config_rv64.mk)
